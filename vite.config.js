b2b6ade3d6603d60e294743657422cc67283010f
susanjd <sue.dunnell@gmail.com>
2026-04-27 13:12:03 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  }
+  plugins: [react()]
 })
